e7915c2ec52c5bffbcd535013396059b9e3500f9
Ankit Kaushik <ankitkaushik6269@gmail.com>
2024-05-04 18:20:00 +0530

    done

--- utils/KindeConfig.js
+++ utils/KindeConfig.js
@@ -2,7 +2,7 @@ import { KindeSDK } from "@kinde-oss/react-native-sdk-0-7x";
 
 export const client = new KindeSDK(
     'https://akimgs.kinde.com',
-    'exp://192.168.0.107:8081',
+    'akimgs:///',
     'b1158ddd01ee4a33bda11fffd7ad3498',
-    'exp://192.168.0.107:8081',
+    'akimgs:///',
 )
